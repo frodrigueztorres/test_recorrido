e41a0c35f0fe8bb5dab6076fa51f8ab790a380c9
Felipe Rodriguez Torres <felipe@rayoslab.com>
2021-09-28 02:26:42 -0300

    adding fetching method

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,8 +1,11 @@
 class Alert < ApplicationRecord
   enum bus_class: { premium: 1, bed_room: 2, semi_room: 3, pullman: 4, other: 5 }
 
-  private
-  # def fetch_id :city_name
-  #   Recorrido::Api.get_cities[:cities].select { |k,v| k[:name] == :city_name }[0][:id]
-  # end
+  def fetch_id_origin
+    Recorrido::Api.get_cities[:cities].select { |k,v| k[:name] == self.city_origin }[0][:id]
+  end
+
+  def fetch_id_destination
+    Recorrido::Api.get_cities[:cities].select { |k,v| k[:name] == self.city_destination }[0][:id]
+  end
 end
--- app/models/recorrido/api.rb
+++ app/models/recorrido/api.rb
@@ -23,5 +23,28 @@ module Recorrido
       response = http.request(request)
       JSON.parse(response.read_body).with_indifferent_access
     end
+
+    def self.bus_travel alert:
+      url = URI("#{ENDPOINT}/es/bus_travels.json")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Post.new(URI(url.to_s))
+      request["Accept"] = "application/json"
+      request["Content-Type"] = "application/json"
+      request.basic_auth("recorrido", "recorrido")
+
+      request.body = {
+        bus_travel: {
+          departure_city_id: alert.fetch_id_origin,
+          destination_city_id: alert.fetch_id_destination,
+          departure_date: I18n.l(alert.updated_at.to_date)
+        }
+      }.to_json
+
+      response = http.request(request)
+      JSON.parse(response.read_body).with_indifferent_access
+    end
   end
 end
